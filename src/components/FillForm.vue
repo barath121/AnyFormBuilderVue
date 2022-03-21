7dc0e0b9f0323703e782c995c750ceca946446e3
barath121 <nadar.barath18@siesgst.ac.in>
2022-03-22 01:50:09 +0530

    Created Seperated Frontend Repo

---
+++ src/components/FillForm.vue
@@ -0,0 +1,114 @@
+<template>
+<form @submit.prevent="submitResponse()" enctype="multipart/form-data">
+    <div class="borderTop colorWhite h-screen overflow-y-scroll snap-y snap-mandatory" v-if="pages.length&&!formFilled">
+        <div v-for="(page,index) in pages" :key="page.id">
+                <Formpage  :formFilled="formFilled" :page="page" :isLastPage="isLastPage(index)"/>
+        </div>
+    </div>
+     <div
+    class="flex flex-col questionpagepreview colorWhite rounded justify-center items-center m-auto" v-if="formFilled">
+    <h1 class="text-3xl break-all">Thanks for filling the form🙌🙌🙌.</h1>
+    </div>
+    <div
+    class="flex flex-col questionpagepreview colorWhite snap-center rounded justify-center items-center m-auto" v-if="!pages.length">
+    <h1 class="text-3xl break-all">This Form is no currently accepting any responses.</h1>
+    </div>
+</form>
+</template>
+<style>
+@import "./../assets/CSS/formbuilder.css";
+@import "./../assets/CSS/index.css";
+</style>
+<script>
+import Formpage from "./FillForm/FormPage.vue";
+import ToastMixin from "./../mixins/toast.js";
+export default {
+  name: "FillForm",
+  components: {
+    Formpage
+  },
+  props: {
+    page : {
+      pageType: { required: true, type: String },
+      question: { default: false, type: String },
+      choices: [{ required: true, type: String }],
+      isRequired : { required: true, type: Boolean },
+      id : {required : true , type : Boolean},
+      fieldName : {required : true , type : String}
+    }
+  },
+  data() {
+    return {
+      pages :[],
+      formFilled : false,
+    };
+  },
+  methods: {
+      submitResponse(){
+          const responseData  = new FormData();
+
+          this.pages.forEach(page=>{
+              if(page.pageType=="Statement")return;
+              let inputValue = document.getElementsByName(page.fieldName)
+              if(page.pageType=="File"){
+                responseData.append(page.fieldName,inputValue[0].files[0]);
+              }
+              else if(page.pageType=="Choice Box"||page.pageType=="Radio Button"){
+                let choicesStr = ""
+                inputValue.forEach(choices=>{
+                    if(choices.checked )
+                       choicesStr  = choicesStr + "," +choices.value;
+                })
+                if(choicesStr.length)
+                choicesStr = choicesStr.substring(1);
+                responseData.append(page.fieldName,choicesStr);
+              }
+              else  
+              responseData.append(page.fieldName,inputValue[0].value);
+        })
+        responseData.append("formID",this.$route.params.id);
+        fetch(`${import.meta.env.VITE_API_URL}/form/saveformresponse`, {
+            method: 'POST',
+            body: responseData  
+        }) 
+        .then(async (result) => {
+        this.formFilled = true;
+        })
+        .catch((err) => {
+          this.displayToast("error", "Some Internal Error");
+        });
+        // localStorage[this.$route.params.id+"FilledForm"] = true;
+      },
+      getPages() {
+      if(localStorage[this.$route.params.id+"FilledForm"] == true)
+        this.formFilled = true;
+      fetch(
+        `${import.meta.env.VITE_API_URL}/form/getpublishedpages/${
+          this.$route.params.id
+        }`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then(async (result) => {
+          let forms = await result.json();
+          this.pages = forms.formData.publishedPages;
+          this.formID = forms.formData._id;
+        })
+        .catch((err) => {
+          this.displayToast("error", "Some Internal Error");
+        });
+    },
+      isLastPage(index){
+          return this.pages.length -1 == index
+      }
+  },
+  mounted() {
+    this.getPages();
+  },
+  mixins : [ToastMixin]
+};
+</script>
---
+++ src/components/FillForm/FormPage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    class="flex flex-col questionpagepreview snap-center rounded justify-center items-center m-auto"
+  >
+    <h1 class="text-3xl break-all overflow-auto hidescroll">{{page.question}}{{isRequired()}}</h1>
+    <input
+      class="w-3/5 questionInput p-2"
+      v-if="page.pageType == 'Small Text'"
+      :name="page.fieldName"
+      :pattern="page.regex"
+      :maxlength="page.maxCharacters"
+      :id="page.fieldName"
+      :required="page.isRequired"
+    />
+    <textarea
+      class="w-3/5 questionTextinput rounded resize-none p-2"
+      v-else-if="page.pageType == 'Large Text'"
+      :name="page.fieldName"
+      :maxlength="page.maxCharacters"
+      :id="page.fieldName"
+      :required="page.isRequired"
+    ></textarea>
+    <div
+      class="flex flex-col overflow-y-auto hidescroll"
+      v-else-if="page.pageType == 'Check Box'"
+    >
+      <div v-for="(choice, index) in page.choices" :key="choice + index">
+       <input  type="checkbox" :name="page.fieldName" :value="choice" :id="page.fieldName+index"/>
+        <label class="overflow-auto hidescroll break-all">{{ choice }}</label>
+      </div>
+    </div>
+    <div
+      class="flex flex-col overflow-y-auto hidescroll"
+      v-else-if="page.pageType == 'Radio Button'"
+    >
+      <div v-for="(choice, index) in page.choices" :key="choice + index">
+        <input  type="radio" :name="page.fieldName" :value="choice" :id="page.fieldName+index" v-if="index==0"  :required="page.isRequired"/>
+        <input  type="radio" :name="page.fieldName"  :value="choice" :id="page.fieldName+index" v-else/>
+        <label class="overflow-auto hidescroll break-all">{{ choice }}</label>
+      </div>
+    </div>
+    <input type="date" v-else-if="page.pageType == 'Date'" :name="page.fieldName" :id="page.fieldName" :required="page.isRequired"/>
+    <input
+      class="w-3/5 fileinput rounded"
+      type="file"
+      v-else-if="page.pageType == 'File'"
+      :name="page.fieldName"
+      :id="page.fieldName"
+      :required="page.isRequired"
+    />
+    <button type="submit" class="buttonalt rounded  m-4 growAnimation" v-if="isLastPage" >Submit</button>
+    <button type="button" class="buttonalt rounded  m-4 growAnimation" v-else @click="goToNext($event)">Next</button>
+  </div>
+</template>
+<script>
+export default {
+  name : "FormPage",
+  props: ["page","isLastPage","formFilled"],
+  methods :{
+      goToNext(event){
+        event.target.parentElement.parentElement.nextSibling.firstChild.scrollIntoView({behavior: "smooth"})
+      },
+      isRequired(){
+        return this.page.isRequired?"*":"";
+      }
+  }
+};
+</script>
